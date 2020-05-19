74728750cd7eaec94a299d849e1de5840c24bd19
ivaz28 <ivaz3823@gmail.com>
2020-05-19 14:21:37 +0300

    add application and main

--- v5/Printers/blenderPrinter/main.py
+++ v5/Printers/blenderPrinter/main.py
@@ -1,23 +1,3 @@
-from frame import *
-from printer import BlenderPrinter
-import json
-from setup import Setup
+from application import appRun
 
-
-setup = Setup()
-setup.load()
-
-frames = JsonFileToFrames(setup.inputFile)
-print('Frames count = '+str(len(frames)))
-
-printer = BlenderPrinter(len(frames), setup.outputDir, setup.FPS)
-#print(sim.getGrowMatrix())
-
-
-iter = 0
-for frame in frames:
-    printer.printFrame(frame, iter + 1)
-    iter += 1
-    
-    #print('#' + str(iter))
-    #print(sim.getGrowMatrix())
+appRun()
